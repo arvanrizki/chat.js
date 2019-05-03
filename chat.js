95ee628718931c2f3473fb5bea412b34923a4374
arvanrizki <50239093+arvanrizki@users.noreply.github.com>
2019-05-03 20:58:38 +0700

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Chat</title>
+</head>
+<body>
+<script id="cid0020000217839335866" data-cfasync="false" async="async" src="//st.chatango.com/js/gz/emb.js" style="width: 303px;height: 351px;" >{"handle":"wibucafe","arch":"js","styles":{"a":"357ae8","b":100,"c":"000000","d":"000000","k":"357ae8","l":"357ae8","m":"357ae8","p":"10","q":"357ae8","r":100,"pos":"br","cv":1,"cvbg":"357ae8","cvw":294,"cvh":28,"ticker":1,"fwtickm":1}}</script>
+</body>
+</html>
